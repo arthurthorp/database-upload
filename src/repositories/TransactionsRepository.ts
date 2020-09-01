72b363c321e23682ff8231e6369c49ae14dffdb0
Arthur Tavares <arthurthorp@hotmail.com>
2020-08-31 21:12:35 -0300

    Import, create and delete transactions

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,34 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = await this.find();
+    const balance = transactions.reduce(
+      (soma: Balance, transaction: Transaction) => {
+        switch (transaction.type) {
+          case 'income':
+            // eslint-disable-next-line no-param-reassign
+            soma.income += Number(transaction.value);
+            break;
+          case 'outcome':
+            // eslint-disable-next-line no-param-reassign
+            soma.outcome += Number(transaction.value);
+            break;
+          default:
+            break;
+        }
+
+        return soma;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    balance.total = balance.income - balance.outcome;
+
+    return balance;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,62 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository, getRepository } from 'typeorm';
 
+import AppError from '../errors/AppError';
+
+import Category from '../models/Category';
 import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    if (!['income', 'outcome'].includes(type)) {
+      throw new AppError('Invalid type of transaction');
+    }
+
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    if (type === 'outcome') {
+      const { total } = await transactionsRepository.getBalance();
+      if (value > total) {
+        throw new AppError('Outcome transaction without a valid balance');
+      }
+    }
+
+    const categoriesRepository = getRepository(Category);
+
+    let categoryData = await categoriesRepository.findOne({
+      where: { title: category },
+    });
+
+    if (!categoryData) {
+      categoryData = categoriesRepository.create({
+        title: category,
+      });
+
+      await categoriesRepository.save(categoryData);
+    }
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category: categoryData,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
